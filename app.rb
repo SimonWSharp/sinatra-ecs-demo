57e85ddb4625575aae58658f468e091346b9fe09
SimonWSharp <simon.w.sharp@gmail.com>
2021-11-22 04:59:37 -0800

    simple endpoint

--- app.rb
+++ app.rb
@@ -1,10 +1,20 @@
 # frozen_string_literal: true
 
 require 'sinatra'
+require 'json'
 
 # Simple Example API
 class BasicApi < Sinatra::Base
+  set :default_content_type, 'application/json'
+
   get '/' do
-    { message: 'hello world' }
+    data = { message: 'hello world' }
+    json data
+  end
+
+  protected
+
+  def json(data)
+    data.to_json
   end
 end
